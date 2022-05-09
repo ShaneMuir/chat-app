2942b6871c36f1495161d6e2ed4cd79631d0b4f0
Shane M <shanem@richs-mbp.home>
2022-05-09 20:05:22 +0100

    added search functionality

--- dist/js/users.js
+++ dist/js/users.js
@@ -25,3 +25,24 @@ setInterval(() => {
   }
   xhr.send();
 }, 500);
+
+searchBar.onkeyup = () => {
+  let searchTerm = searchBar.value;
+  if(searchTerm != "") {
+    searchBar.classList.add('active');
+  } else {
+    searchBar.classList.remove('active');
+  }
+  let xhr = new XMLHttpRequest();
+  xhr.open("post", "php/search.php", true);
+  xhr.onload = () => {
+    if(xhr.readyState === XMLHttpRequest.DONE) {
+      if(xhr.status === 200) {
+        let data = xhr.response;
+        usersList.innerHTML = data;
+      }
+    }
+  }
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhr.send('searchTerm=' + searchTerm);
+}
