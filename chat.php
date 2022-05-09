a4f9fd0df9ee363d6018a2bde56fc33c8cb2fd76
Shane M <shanem@richs-mbp.home>
2022-05-09 20:18:04 +0100

    added the chat functionality

--- chat.php
+++ chat.php
@@ -1,23 +1,41 @@
-<?php include_once('header.php'); ?>
-<div class="wrapper">
-    <section class="chat-area">
-        <header>
-            <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-            <img src="https://via.placeholder.com/50" alt="">
-            <div class="details">
-                <span>Shane Muir</span>
-                <p>Active Now</p>
-            </div>
-        </header>
-        <div class="chat-box">
+<?php
+session_start();
+include_once "php/config.php";
+if(!isset($_SESSION['unique_id'])) {
+    header('location: login.php');
+}
+include_once('header.php');
+?>
+    <div class="wrapper">
+        <section class="chat-area">
+            <header>
+                <?php
+                $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+                if(mysqli_num_rows($sql) > 0){
+                    $row = mysqli_fetch_assoc($sql);
+                }else{
+                    header("location: users.php");
+                }
+                ?>
+                <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="php/images/<?php echo $row['img']; ?>" alt="">
+                <div class="details">
+                    <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+                    <p><?php echo $row['status']; ?></p>
+                </div>
+            </header>
+            <div class="chat-box">
 
-        </div>
-        <form action="#" class="typing-area">
-            <input type="text" class="incoming_id" name="incoming_id" value="#" hidden>
-            <input type="text" name="message" class="input-field" placeholder="Type a message here..." autocomplete="off">
-            <button><i class="fab fa-telegram-plane"></i></button>
-        </form>
-    </section>
-</div>
+            </div>
+            <form action="#" class="typing-area">
+                <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
+                <input type="text" name="message" class="input-field" placeholder="Type a message here..." autocomplete="off">
+                <button><i class="fab fa-telegram-plane"></i></button>
+            </form>
+        </section>
+    </div>
+
+    <script src="dist/js/chat.js"></script>
 
 <?php include_once('footer.php'); ?>
