910e084e4e2a5adc6d9d8070fec7d8b57bb593a6
Shane Muirhead <shane.muirhead@tribusdigital.com>
2022-08-10 14:46:18 +0100

    added db

--- php/config.php
+++ php/config.php
@@ -1,8 +1,14 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', 'root', 'chat_app');
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+
+
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 if(!$conn) {
     echo "Error: " . mysqli_connect_error();
-} else {
-    echo "connected!";
 }
