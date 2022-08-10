01f0efb14af8709da5c99aeca3d2aabc25a4fd48
Shane Muirhead <shane.muirhead@tribusdigital.com>
2022-08-10 15:50:16 +0100

    Added new db schema + some minor fixes

--- php/config.php
+++ php/config.php
@@ -1,14 +1,20 @@
 <?php
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
+// Set mode for production / development
+const MODE = 'development';
 
+if(MODE == 'production'):
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+else:
+    $conn = mysqli_connect('localhost', 'root', 'root', 'chat-app');
+endif;
 
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 if(!$conn) {
     echo "Error: " . mysqli_connect_error();
 }
--- php/logout.php
+++ php/logout.php
@@ -7,12 +7,14 @@
         if(isset($logout_id)) {
             $status = "Offline now";
             $time_now = time();
-            echo $time_now;
             $sql = mysqli_query($conn, "UPDATE users SET status = '{$status}', last_active = '{$time_now}' WHERE unique_id = '{$logout_id}'");
             if($sql) {
                 session_unset();
                 session_destroy();
                 header('location: ../login.php');
+            }
+            if(!$sql) {
+                echo("Error description: " . mysqli_error($conn));
             }
         } else {
             header('location: ../users/php');
